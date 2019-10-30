f063af5498a6cf2ebfc3f23eba0edc6246d3d09d
STEVEN JOYCE - STUDENT <G00362012@gmit.ie>
2019-10-30 10:37:25 +0000

    first commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,6 +4,10 @@ const port = 4000
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+//set the database url as a var and connect to it with mongoose
+const mongoDB = 'mongodb+srv://steven:London19972009@cluster0-balhd.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, {useNewUrlParser:true});
 
 //Allow the app to access the local server
 app.use(cors());
@@ -21,6 +25,17 @@ app.use(bodyParser.urlencoded({ extended: false}))
 //parse application/json
 app.use(bodyParser.json())
 
+//add a schema
+const Schema = mongoose.Schema;
+//store data in a schema
+const movieSchema = new Schema({
+    title:String,
+    year:String,
+    poster:String
+})
+
+//Create model to add and delete data from database in a collection
+const MovieModel = mongoose.model('movies',movieSchema);
 
 //To find on browser call http://localhost:4000/hello
 app.get('/hello', (req, res) => res.send('Hello'))
@@ -35,7 +50,11 @@ app.get('/hello/:name',(req,res) => {
 //Call the data and send it to the server
 //It runs on http://localhost:4000/api/movies
 app.get('/api/movies',(req,res) => {
-    const myMovies = [
+    //get data from collection in database
+    MovieModel.find((error,data)=>{
+        res.json({movies:data});
+    })
+   /* const myMovies = [
         {
         "Title":"Avengers: Infinity War",
         "Year":"2018",
@@ -50,16 +69,31 @@ app.get('/api/movies',(req,res) => {
         
      res.status(200).json({movies:myMovies,
                             message: 'Operation completed successfully'
-                        })
+                        })*/
    
 })
 //POST to send name, year and url of poster 
+//Use MovieModel to send data to database and create a collection to store that data
 app.post('/api/movies', (req, res)=> {
     console.log('Movie recieved')
     console.log(req.body);
     console.log(req.body.title);
     console.log(req.body.year);
     console.log(req.body.poster);
+    MovieModel.create({
+        title:req.body.title,
+        year:req.body.year,
+        poster:req.body.poster
+    });
+})
+//Method to get id
+app.get('/api/moives/:id', (req,res)=> {
+    console.log(req.params.id);
+
+    //Use id to find a specific id
+    MovieModel.findById(req.params.id,(error,data)=>{
+        res.json(data);
+    })
 })
 //This sends a local html file to browser
 //path variable finds the complete file location and sends it to the server
--- src/components/movies.js
+++ src/components/movies.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import MovieItem from './movieItem';
+import Card from 'react-bootstrap/Card'
 
 class Movies extends React.Component {
   //To turn function to class add class name and extend app to include React.Component
   //Put the return in a render
-  render(){
-    //This goes and pulls all data from the mtMovies JSON and execute it in this component
-    //The map function splits the array into 4 parts(4 movies) and outputs it
-    return this.props.myMovies.map((movie)=>{
-        console.log(movie);
-       return <MovieItem movie={movie}> </MovieItem>
-    });
-  }
+  render() {
+    return (
+    <div>
+    {/* <h4>{this.props.movie.Title}</h4>
+    <p>{this.props.movie.Year}</p>
+    <img src={this.props.movie.Poster} width="200" height="200"></img>
+    */}
+    <Card>
+    <Card.Header>{this.props.movie.title}</Card.Header>
+    <Card.Body>
+    <blockquote className="blockquote mb-0">
+    <img src={this.props.movie.poster} width="200" height="200"></img>
+    <footer>
+    {this.props.movie.year}
+    </footer>
+    </blockquote>
+    </Card.Body>
+    </Card>
+    </div>
+    );
+    }
   
 }
 
